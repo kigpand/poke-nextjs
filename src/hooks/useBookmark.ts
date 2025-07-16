526a6efb328fda14ba241eda0194e5ee59faa5f1
김지훈 <kigpand@gmail.com>
2025-07-16 22:26:37 +0900

    feat: 도감 store, hook 생성

---
+++ src/hooks/useBookmark.ts
@@ -0,0 +1,32 @@
+import { IPokemon } from "@/interface/IPokemon";
+import { useBookmarkStore } from "@/store/bookmarkStore";
+
+/**
+ * 도감용 훅
+ */
+export function useBookmark() {
+  const { bookmarkList, addBookmarkItem, removeBookmarkItem } =
+    useBookmarkStore();
+
+  function addBookmark(pokeItem: IPokemon, callback: Function) {
+    const result = bookmarkList.find(
+      (pokeList: IPokemon) => pokeList.id === pokeItem.id
+    );
+    if (result) {
+      alert("이미 도감에 등록된 포켓몬입니다.");
+      return;
+    }
+    addBookmarkItem(pokeItem);
+    callback();
+  }
+
+  function findBookmark(poke: IPokemon) {
+    return bookmarkList.some((list: IPokemon) => poke.id === list.id);
+  }
+
+  function removeBookmark(item: IPokemon) {
+    removeBookmarkItem(item);
+  }
+
+  return { addBookmark, findBookmark, removeBookmark };
+}
