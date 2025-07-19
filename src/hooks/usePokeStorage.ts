dd81ffe2ead1605ad1d5a4c0a987bc82ffe3cfd4
김지훈 <kigpand@gmail.com>
2025-07-19 22:59:41 +0900

    refactor: storage 관련 훅 type, pokemon용 분리

--- src/hooks/usePokeStorage.ts
+++ src/hooks/usePokeStorage.ts
@@ -1,6 +1,4 @@
-import { typeConvertDamegeData } from "@/utils/converter";
 import { useCallback } from "react";
-import types from "@/json/types.json";
 
 export function usePokeStorage() {
   const setCurrentPokeStorage = useCallback(() => {}, []);
@@ -14,27 +12,9 @@ export function usePokeStorage() {
     sessionStorage.removeItem("currentPoke");
   }, []);
 
-  const setTypeStorage = useCallback((type: string) => {
-    sessionStorage.setItem("type", type);
-  }, []);
-
-  const getTypeStorage = useCallback(() => {
-    const sessionType = sessionStorage.getItem("type");
-    if (!sessionType) return null;
-    const type = types.find((type) => type.name === sessionType);
-    return type ? typeConvertDamegeData(type) : null;
-  }, []);
-
-  const clearTypeStorage = useCallback(() => {
-    sessionStorage.removeItem("type");
-  }, []);
-
   return {
     setCurrentPokeStorage,
-    setTypeStorage,
     clearCurrentPokeStorage,
-    clearTypeStorage,
     getCurrentPokeStorage,
-    getTypeStorage,
   };
 }
