211a67dbc02684e21e2bc457afa74dca631f188b
김지훈 <kigpand@gmail.com>
2025-09-04 22:05:20 +0900

    feat: 로딩 스피너, 페이지전환 프로그레스바 작업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import RouteProgressBar from "./provider/RouteProgressBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <RouteProgressBar />
         {children}
         <div id="overlay-root"></div>
       </body>
