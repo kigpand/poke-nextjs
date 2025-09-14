9257ca1f8c1de91709844a7c2387bf8ee68805f5
김지훈 <kigpand@gmail.com>
2025-09-14 21:23:32 +0900

    feat: 정렬 버튼 테스트 코드 추가

--- src/components/home/button/SortButton.tsx
+++ src/components/home/button/SortButton.tsx
@@ -9,7 +9,11 @@ export function SortButton() {
   return (
     <>
       <button
+        type="button"
         aria-label="필터링 모달 호출 버튼"
+        aria-haspopup="dialog"
+        aria-expanded={isOpen}
+        aria-controls="sort-modal"
         className="w-[40px] h-[40px] bg-background cursor-pointer border border-gray-400 p-1 rounded-xl hover:bg-gray-100"
         onClick={open}
       >
