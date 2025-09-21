1668b36a738b6a79136f2445716476661fe3684b
김지훈 <kigpand@gmail.com>
2025-09-21 20:33:55 +0900

    feat: 프로그레스바제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import RouteProgressBar from "./provider/RouteProgressBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <RouteProgressBar />
         {children}
         <div id="overlay-root"></div>
       </body>
